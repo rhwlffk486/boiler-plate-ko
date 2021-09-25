0222a0c3d292d6e2a682d3a6463f3f8562e9be0e
rhwlffk486 <taehun0023@gmail.com>
2021-09-25 20:22:43 +0900

    hi

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT;
- 
-var o = {} // empty Object
-var key = 'reponse';
-o[key] = []; // empty Array, which you can push() values into
- 
- 
-var data = {
-    name: 'jason',
-    age: '33'
-};
-var data2 = {
-    name: 'mike',
-    data: '34'
-};
-o[key].push(data);
-o[key].push(data2);
- 
-var result = JSON.stringify(o);
- 
-app.get("/", (req, res) => {
-res.send(result);
-});
-app.listen(port);
+const express = require('express')
+const app = express()
+const port = 5000
+// express 4.16 버전 이상부터는 bodyparser을 지원하기 때문에 express.urlencoded 로 사용해야한다.
+// const bodyparser = require('body-parser')
+const mongoose = require('mongoose')
+const { User } = require('./models/User');
+
+const config = require('./config/key');
+const { mongoURI } = require('./config/dev');
+
+// 데이터를 분석해서 가져온다.
+app.use(express.urlencoded({extended: true}))
+
+// json 타입으로 된 데이터를 분석해서 가져온다
+app.use(express.json());
+
+// mongoose.connect(config, mongoURI, {
+//     useNewUrlParser: true, useUnifiedTopology: true
+// }).then(() => console.log('MongoDB Connected...'))
+//   .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello eWorld'))
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
