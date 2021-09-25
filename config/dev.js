37ae14e7b79cff460dc321a36664e0aaf0e40228
rhwlffk486 <taehun0023@gmail.com>
2021-09-25 21:18:14 +0900

    비밀 정보 보호

--- config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports ={
-    mongoURI: "mongodb+srv://taehun0023:Dlxogns65143!@cluster0.2ybus.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-}
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 5000
 // express 4.16 버전 이상부터는 bodyparser을 지원하기 때문에 express.urlencoded 로 사용해야한다.
 // const bodyparser = require('body-parser')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const { User } = require('./models/User');
 
 const config = require('./config/key');
@@ -15,10 +15,10 @@ app.use(express.urlencoded({extended: true}))
 // json 타입으로 된 데이터를 분석해서 가져온다
 app.use(express.json());
 
-// mongoose.connect(config, mongoURI, {
-//     useNewUrlParser: true, useUnifiedTopology: true
-// }).then(() => console.log('MongoDB Connected...'))
-//   .catch(err => console.log(err))
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World'))
 
